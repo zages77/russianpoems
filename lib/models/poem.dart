ac80ed17f146d42d28dc1f33f11643cf97fd4799
Egor <yegsz747@gmail.com>
2022-12-07 00:10:59 +0300

    set

---
+++ lib/models/poem.dart
@@ -0,0 +1,8 @@
+class Poem {
+  final String id;
+  final String title;
+  final String author;
+  final String text;
+
+  Poem({required this.id, required this.title, required this.author, required this.text,});
+}
